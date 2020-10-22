311a2ab9a985c9b97569c5f031ffe67781b6c57b
wangwangla <2818815189@qq.com>
2020-10-22 23:36:42 +0800

    baouc

--- assets/Script/GameScene.ts
+++ assets/Script/GameScene.ts
@@ -1,4 +1,5 @@
 import GameCtrl from "./GameCtroll";
+import GameView from "./GameView";
 
 const {ccclass, property} = cc._decorator;
 
@@ -8,11 +9,19 @@ export default class GameScene extends cc.Component {
     pokerPrefab:cc.Prefab;
     @property(cc.Node)
     container:cc.Node;
+    @property(cc.Prefab)
+    private gameViewUI:cc.Prefab;
+
+    private gameView : GameView = null;
 
 
     private g_gameCtrl : GameCtrl = null;
 
     start () {
+        //管理者
+        let gameViewUINode = cc.instantiate(this.gameViewUI);
+        this.gameView = gameViewUINode.getComponent(GameView);
+        this.node.addChild(this.gameView.node);
         this.g_gameCtrl = new GameCtrl();
         this.g_gameCtrl.start(this.pokerPrefab,this.container);
     }
---
+++ assets/Script/GameView.ts
@@ -0,0 +1,9 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class GameView extends cc.Component {
+
+
+    start () {
+    }
+}
